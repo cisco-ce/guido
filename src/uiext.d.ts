e61e7b945122ed1eaf442b7b837fe79c7b8974ef
Tore Bjolseth <tbjolset@cisco.com>
2021-07-10 22:11:22 +0200

    update uiext.d.ts file to match uiext.ts

--- src/uiext.d.ts
+++ src/uiext.d.ts
@@ -1,74 +1,84 @@
-declare interface Extensions {
-  attributes: ExtensionsAttributes;
-  children: Panel[];
-}
+declare interface Node {}
+declare type Config = Node;
+declare type Panel = Node;
+declare type Page = Node;
+declare type Row = Node;
+declare type Widget = Node;
 
-declare interface ExtensionsAttributes {
+declare function Config(attributes: ConfigAttributes, children?: Panel | Panel[]): Config;
+
+declare function Panel(attributes: PanelAttributes, children?: Page | Page[]): Panel;
+
+declare function Page(attributes: PageAttributes, children: Row | Row[]): Page;
+
+declare function Row(attributes: RowAttributes, children: Widget | Widget[]): Row;
+
+
+declare interface ConfigAttributes {
   version?: string;
-}
-
-declare interface Panel {
-  attributes: PanelAttributes;
-  children: Page[];
 }
 
 declare interface PanelAttributes {
   panelId?: string;
-  order?: number;
   type?: 'Home' | 'InCall' | 'StatusBar' | 'Never';
-  icon?: string;
   color?: string;
+  icon?: string;
+  order?: number;
   name?: string;
-}
-
-declare interface Page {
-  attributes: PageAttributes;
-  children: Row[];
 }
 
 declare interface PageAttributes {
   pageId?: string;
   name?: string;
-  compact?: boolean;
-}
-
-declare interface Row {
-  attributes: RowAttributes;
-  children: Widget[];
+  hideRowNames?: boolean;
 }
 
 declare interface RowAttributes {
   text?: string;
 }
 
-declare interface Attributes {}
-
-declare interface Widget {
-  attributes: Attributes;
-}
-
-declare interface WidgetAttributes extends Attributes {
-  widgetId: string;
-}
 
 declare type WidgetSize = 1 | 2 | 3 | 4;
 
-declare interface ButtonAttributes extends WidgetAttributes {
+declare function Button(attributes: {
+  widgetId: string;
   text?: string;
   size?: WidgetSize;
-}
+  icon?: string;
+}): Widget;
 
-declare function Extensions(attributes: ExtensionsAttributes, children: Panel | Panel[]) : Extensions;
+declare function GroupButton(attributes: {
+  widgetId: string;
+  buttons: object;
+}): Widget;
 
-declare function Panel(attributes: PanelAttributes, children: Page | Page[]) : Panel;
+declare function Spinner(attributes: {
+  widgetId: string;
+  size?: WidgetSize;
+  style?: 'vertical' | 'horizontal' | 'plusminus';
+}): Widget;
 
-declare function Page(attributes: PageAttributes, children: Row | Row[]) : Page;
+declare function Slider(attributes: {
+  widgetId: string;
+  size?: WidgetSize;
+}): Widget;
 
-declare function Row(attributes: RowAttributes, children: Widget | Widget[]) : Row;
+declare function Spacer(attributes: {
+  widgetId: string;
+  size?: WidgetSize;
+}): Widget;
 
-declare function Button(attributes: ButtonAttributes) : Widget;
+declare function DirectionalPad(attributes: {
+  widgetId: string;
+  text?: string;
+}): Widget;
 
-declare function toXml(extensions: Extensions): string;
+declare function Text(attributes: {
+  widgetId: string;
+  text?: string;
+  size?: WidgetSize;
+}): Widget;
 
-
-
+declare function ToggleButton(attributes: {
+  widgetId: string;
+}): Widget;
