fb2348f16407ce4f3da0987b99bff41c8e6b73d0
Tore Bjolseth <tbjolset@cisco.com>
2021-08-09 20:42:22 +0200

    clean up universal adapter

---
+++ src/universal-adapter.js
@@ -0,0 +1,55 @@
+/**
+ * Adapter that makes it possible to run a pure macro from an external node context instead.
+ * Useful to develop, test and commit macros on own laptop instead of in macro editor,
+ * then just copy/paste/export/commit&push the macro file when done.
+ *
+ * Macros on video device can just import the xapi object directly, but when external you need
+ * to provide login details. Some magic here makes lets you connect to the video device with jsxapi,
+ * then require the macro file, which will run as if it had the same xapi object as the video device
+ *
+ * Usage:
+ *  const adapter = require('./universal-adapter');
+ *  const videoDevice = { host: '10.0.0.99', username: 'admin', password: 'password };
+ *  const macroModule = './mymacro'; // this is the file name you would usually use in require()
+ *  adapter(videoDevice, macroModule); // macro starts
+ *
+ */
+
+const jsxapi = require('jsxapi');
+const Module = require('module');
+
+const originalRequire = Module.prototype.require;
+
+function connectXapi ({ host, username, password }) {
+  return new Promise((resolve, reject) => {
+    jsxapi
+    .connect('wss://' + host, {
+      username,
+      password,
+    })
+    .on('error', e => reject(e))
+    .on('ready', async (xapi) => {
+      resolve(xapi);
+    });
+  });
+}
+
+function init(deviceLogin, macroModuleName) {
+
+  connectXapi(deviceLogin)
+    .then((xapi) => {
+
+      Module.prototype.require = () => {
+        const moduleName = arguments[0];
+        return moduleName === 'xapi' ? xapi : originalRequire.apply(this, arguments);
+      };
+
+      // run macro as an external integration. when it calls require('xapi') it will get the connected
+      // object from above
+      require(macroModuleName);
+
+    });
+}
+
+module.exports = init;
+
--- test/test-showcase.js
+++ test/test-showcase.js
@@ -1,29 +1,4 @@
-const jsxapi = require('jsxapi');
-const Module = require('module');
+const universalAdapter = require('../src/universal-adapter');
 
-const originalRequire = Module.prototype.require;
-
-function connectXapi (host, username, password) {
-  return new Promise((resolve, reject) => {
-    jsxapi
-    .connect('wss://' + host, {
-      username,
-      password,
-    })
-    .on('error', e => reject(e))
-    .on('ready', async (xapi) => {
-      resolve(xapi);
-    });
-  });
-}
-
-connectXapi('10.0.0.25', 'tore', 'ynglinge').then((xapi) => {
-  Module.prototype.require = function() {
-    const moduleName = arguments[0];
-    return moduleName === 'xapi' ? xapi : originalRequire.apply(this, arguments);
-  };
-
-  // run macro as an external integration. when it calls require('xapi') it will get the connected
-  // object from above
-  require('../src/showcase');
-});
+const login = { host: '10.0.0.25', username: 'tore', password: 'ynglinge' };
+universalAdapter(login, './showcase');
